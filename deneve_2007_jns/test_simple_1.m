f588c7e716315d45019077161c835fd10877ec30
Cosmo Zhang <cosmozhang1995@gmail.com>
2017-07-03 14:02:04 +0800

    dev

--- deneve_2007_jns/test_simple_1.m
+++ deneve_2007_jns/test_simple_1.m
@@ -1,13 +1,23 @@
+function [] = test_simple_1()
 
-P = 60;
-D = 1;
-C = 0;
-% L = reshape([1 0 0 0.5 0.9 0 0 1 1], [3 3]);
+%% specify the parameters
+
+P = 60; % Number of neurons for per dimension
+D = 1; % Number of sensory dimensions
+C = 0; % Number of controlling dimensions
+
+% Recurrent weights:
+% Concatenate the measurements,
+% i.e. let x_ = [s_1, s_2, ..., s_D, c_1, c_2, ..., c_C, 1]
+% So, rows/cols of L should be D+C+1
 L = [1, 0.003; 0, 1];
+% L = reshape([1 0 0 0.5 0.9 0 0 1 1], [3 3]);
 Ma = L;
 
-iter_times = 300;
-plot_steps = 100;
+iter_times = 300; % steps of recurrent iterations
+plot_steps = 100; % plot a response curve every `plot_steps`
+
+%% do the simulation
 
 prefs = gen_func_preferences(D+C, P);
 pref_ticks = prefs(1,:);
@@ -29,7 +39,6 @@ act = activity_iterate(D, C, init_act, init_sensory, weights, 0, 1);
 figure(1);
 
 subplot(3, 1, 1);
-title('Iterations');
 for i = 1:iter_times
     if mod(i-1, plot_steps) == 0
 %         plot(act, 'Color', [1,1,1]*(i/iter_times)); hold on;
@@ -45,6 +54,7 @@ for i = 1:iter_times
     
     last_actual_val = actual_val(:,i);
 end
+title('Iterations');
 
 subplot(3, 1, 2);
 plot(preds);
@@ -55,3 +65,5 @@ title('Predictions');
 subplot(3, 1, 3);
 plot(preds - actual_val(1,:));
 title('Error');
+
+end
